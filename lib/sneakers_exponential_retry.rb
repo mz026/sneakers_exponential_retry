9bcb3d8a0f559d3b05b67c3d4141690dac0370d6
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2016-08-18 17:23:30 +0800

    init

---
+++ lib/sneakers_exponential_retry.rb
@@ -0,0 +1,5 @@
+require "sneakers_exponential_retry/version"
+
+module SneakersExponentialRetry
+  # Your code goes here...
+end
---
+++ lib/sneakers_exponential_retry/version.rb
@@ -0,0 +1,3 @@
+module SneakersExponentialRetry
+  VERSION = "0.1.0"
+end
